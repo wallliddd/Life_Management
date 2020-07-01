86f77cd4592344fe74f6e810bf9dcd5bb1904dfe
walid <wavzaly@gmail.com>
2020-07-01 13:31:05 +0200

    first commit, react app

---
+++ src/main/java/no/kantega/springandreact2/SpringAndReact2Application.java
@@ -0,0 +1,13 @@
+package no.kantega.springandreact2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndReact2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndReact2Application.class, args);
+	}
+
+}
---
+++ src/test/java/no/kantega/springandreact2/SpringAndReact2ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.kantega.springandreact2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndReact2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
